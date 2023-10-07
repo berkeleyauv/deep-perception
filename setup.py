f7e5c46e8411fac82819f13eeb0c9b0620a9fb66
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2023-10-07 14:43:25 -0700

    Initial comit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deep_perception',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+    },
+    author='URobotics@Berkeley',
+)
